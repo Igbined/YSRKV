d91a73ef132db6ac27ef543424ea9df711deaa0f
Igbined <93369396+Igbined@users.noreply.github.com>
2026-07-04 08:03:17 +0100

    Update config.js

--- JS/config.js
+++ JS/config.js
@@ -1,3 +1,3 @@
 // Shared configuration for JS modules
 //const ENDPOINT = "https://admin-telegram-bot--fortuneigbinedi.replit.app/api/public/endpoint/FYH57";
-const ENDPOINT = "https://telegram-activation-bot--igbinedion109.replit.app/api/OIMUB";
+const ENDPOINT = "https://telegram-activation-bot--igbinedion109.replit.app/api/YSRKV";
